621046c961d3df2891524868a2d333b55bda852d
Rone Shender Loza Aliaga <rone@b89.io>
2021-11-24 17:31:02 -0500

    Initial Commit

---
+++ PrograManiacsSwiftUI/PrograManiacsSwiftUI.h
@@ -0,0 +1,18 @@
+//
+//  PrograManiacsSwiftUI.h
+//  PrograManiacsSwiftUI
+//
+//  Created by Rone Shender on 24/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrograManiacsSwiftUI.
+FOUNDATION_EXPORT double PrograManiacsSwiftUIVersionNumber;
+
+//! Project version string for PrograManiacsSwiftUI.
+FOUNDATION_EXPORT const unsigned char PrograManiacsSwiftUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrograManiacsSwiftUI/PublicHeader.h>
+
+
