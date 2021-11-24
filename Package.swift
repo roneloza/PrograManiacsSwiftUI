cc2cb2c2e6420b3ed3b1bf2b12354c68701cf8ad
Rone Shender Loza Aliaga <rone@b89.io>
2021-11-24 17:38:57 -0500

    Add Package.swift
    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "PrograManiacsSwiftUI",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "PrograManiacsSwiftUI",
+            type: .dynamic,
+            targets: ["PrograManiacsSwiftUI"])
+    ],
+    targets: [
+        .target(
+            name: "PrograManiacsSwiftUI",
+            path: "PrograManiacsSwiftUI"
+        )
+    ]
+)
