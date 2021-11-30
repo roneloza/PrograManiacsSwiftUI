adf28f8ed3b86972470244e2027b949091b67d7a
Rone Shender Loza Aliaga <rone@b89.io>
2021-11-30 18:05:12 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "PrograManiacsSwiftUI",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v13)
     ],
     products: [
         .library(
